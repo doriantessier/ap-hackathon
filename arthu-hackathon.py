11451f29d46f3183890b3fbb8a43486e17436517
ArthurCli <arthur.climaud@etu.minesparis.psl.eu>
2025-01-31 11:55:50 +0100

    Salle finale avec passages qui changent aléatoirem

--- arthu-hackathon.py
+++ arthu-hackathon.py
@@ -18,13 +18,17 @@ def generate_map(width, height):
     xsalle = 1
     nbrsalle = rd.randint(4,9)
     print(nbrsalle)
-    L=[]
+    Lx=[]
+    Llg=[]
+    Lh = []
     for i in range(nbrsalle):
                 
                    hauteur = rd.randint(6,12)
                    largeur = rd.randint(4,9)
                    xsalle += largeur + 3 + rd.randint(3,6)
                    Lx.append(xsalle)
+                   Llg.append(largeur)
+                   Lh.append(hauteur)
                    draw_room(xsalle, 1, largeur, hauteur)
 
     
@@ -33,12 +37,21 @@ def generate_map(width, height):
     #for i in range(len(L)-1):
     #    for j in range(L[i+1][1]-L[i][1]):
     #         grid[5][L[i][1]+ L[i][2] + j] = '#'
-
-    for i in range(L[0], L[-1]):
-        if grid[5][i] == ' ':
-            grid[5][i] = '#'
+    for j in range(nbrsalle-1):
+        minh = min(Lh[j],Lh[j+1])
+        h = rd.randint(2,minh-1)
+        for i in range(Lx[j], Lx[j+1]):
+            if grid[h][i] == ' ':
+                grid[h][i] = '#'
+            #elif grid[h][i] == '|':
+            #    grid[h][i] = '='
+            #    grid[h][Lx[0]] = '|'
+            #    grid[h][Lx[-1]] = '='
+            if j != nbrsalle-1:
+                 grid[h][Lx[j]+Llg[j]-1]= '='
+                 grid[h][Lx[j+1]]= '='
     
-    return grid, nbrsalle, L
+    return grid, nbrsalle, Lx
 
 def print_map(grid):
     for row in grid:
@@ -46,8 +59,8 @@ def print_map(grid):
 
 if __name__ == "__main__":
     width, height = 140, 20  # Taille de la carte
-    dungeon_map , nbrsalle, L= generate_map(width, height)
+    dungeon_map , nbrsalle, Lx= generate_map(width, height)
     print_map(dungeon_map)
-    print(nbrsalle, L)
+    print(nbrsalle, Lx)
 
 
